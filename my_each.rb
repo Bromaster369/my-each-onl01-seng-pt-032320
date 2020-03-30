ad2176e25d1ffe37629758b98e69ca36add96f9e
Christian Pardo <chrispardopersonal@gmail.com>
2020-03-30 17:48:13 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
-def my_each(array) # put argument(s) here
+def my_each(array) 
   i = 0 
   while i < array.length
     yield(array[i])
     i = i + 1
   end
-  collection  # code here
+  collection  
 end
